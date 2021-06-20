f53d8114e44d1460169c2d9d782a3a76436f44fe
Keith F. Prussing <Keith.Prussing@gtri.gatech.edu>
2021-06-20 02:10:51 -0400

    Add a test session to make sure it all works
    The end goal is to make sure the tool just works.  Instead of hand
    running the example, we can add a test session to the nox file.  It just
    makes sure the generated HTML is what is expected.  We cannot check the
    graphic because matplotlib drifts too much.
    
    And while we're at it, we stop ignoring the reference example files but
    ignore the .nox directory.

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,7 @@
+import filecmp
+import pathlib
+import shutil
+
 import nox
 
 
@@ -6,3 +10,30 @@ def flake8(session):
     """Run flake8"""
     session.install("flake8")
     session.run("flake8", "__init__.py", "noxfile.py")
+
+
+@nox.session(python=["3.5", "3.6", "3.7", "3.8", "3.9"],
+             venv_backend="conda")
+def test(session):
+    session.install("scons", "panflute")
+    session.conda_install("pandoc")
+    # Document building
+    session.install("numpy", "matplotlib")
+    root = pathlib.Path(__file__).parent
+    dest = pathlib.Path(session.bin).parent
+
+    tooldir = dest / "site_scons" / "site_tools"
+    tooldir.mkdir(parents=True, exist_ok=True)
+    shutil.copy("__init__.py", tooldir / "pandoc.py")
+
+    for src in root.joinpath("example").glob("*"):
+        if src.suffix in (".html", ".png"):
+            continue
+
+        shutil.copy(src, dest)
+
+    session.chdir(dest)
+    session.run("scons", external=False)
+    assert filecmp.cmp(root / "example" / "example.html",
+                       dest / "example.html",
+                       shallow=False)
