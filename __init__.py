6eae48ec159c38a1fddeacae590f426b85e41d06
Keith F. Prussing <Keith.Prussing@gtri.gatech.edu>
2021-06-20 01:28:05 -0400

    Update Warning to SConsWarning
    In version 4, Warning was changed to SConsWarning.

--- __init__.py
+++ __init__.py
@@ -73,7 +73,7 @@ else:
     logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 
-class ToolPandocWarning(SCons.Warnings.Warning):
+class ToolPandocWarning(SCons.Warnings.SConsWarning):
     pass
 
 
